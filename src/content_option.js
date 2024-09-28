0fb1f5184a27fa7fbdbe0585deb2f9fab405ad58
hammadyozae <hammad@yozae.com>
2024-09-29 02:58:28 +0500

    portfoil

---
+++ src/content_option.js
@@ -0,0 +1,266 @@
+const logotext = "Hammad";
+const meta = {
+    title: "Hammad Hussain",
+    description: "I’m Hammad Hussain Full stack developer,currently working in Pakistan",
+};
+
+const introdata = {
+    title: "I’m Hammad Hussain",
+    animated: {
+        first: "Are you looking for a professional developer?",
+        second: "With expertise in 8 to 9 programming languages?",
+        third: "And proficient in deploying applications using DevOps methodologies?",
+        fourth: "Feel free to review my portfolio and learn more about me through the links below.",
+    },
+    description: "",
+    your_img_url: "mains.png",
+};
+
+const dataabout = {
+    title: "a bit about my self",
+    aboutme: "I am an experienced full-stack developer with over 5 years of expertise in web and mobile application development, cloud solutions (AWS, Digital Ocean), web services, and microservices architecture. My focus is on building robust, scalable, and efficient solutions that meet client requirements and exceed expectations. I specialize in both backend and frontend development, ensuring a seamless experience from design to deployment. I’m proficient in building modern, user-centric applications and have a deep understanding of various frontend frameworks, responsive design techniques, and best coding practices. Whether it’s a simple website or a complex mobile application, I deliver quality work on time.",
+};
+const worktimeline = [{
+    jobtitle: "Laravel/PHP Intern",
+    where: "Evantagesoft",
+    to: "12-Feb-2019",
+    date: "30-July-2019",
+},
+    {
+        jobtitle: "Laravel/php developer",
+        where: "Marten Digitals",
+        to: "01-Aug-2019",
+        date: "30-Sep-2020",
+    },
+    {
+        jobtitle: "Mern Stack & Wordpress Custom Plugins Developer",
+        where: "Qfnetwork",
+        to: "02-Oct-2020",
+        date: "20-Jan-2022",
+    },
+    {
+        jobtitle: "Senior Full Stack Developer",
+        where: "ItCoderzsolution",
+        to: "25-Jan-2022",
+        date: "27-Feb-2024",
+    },
+    {
+        jobtitle: "Senior Full Stack Developer",
+        where: "Yozae",
+        to: "01-Mar-2024",
+        date: "Present",
+    },
+];
+
+const skills = [{
+    name: "Laravel/PHP",
+    value: 100,
+},
+    {
+        name: "ReactJS",
+        value: 100,
+    },
+    {
+        name: "Javascript",
+        value: 100,
+    },
+    {
+        name: "NodeJS",
+        value: 100,
+    },
+    {
+        name: "Jquery",
+        value: 100,
+    },
+    {
+        name: "HTML & CSS",
+        value: 100,
+    },
+    {
+        name: "Dockerization Apps",
+        value: 80,
+    },
+    {
+        name: "AWS",
+        value: 80,
+    },
+    {
+        name: "DNS Settings",
+        value: 100,
+    },
+    {
+        name: "CloudFlare",
+        value: 100,
+    },
+    {
+        name: "Flutter",
+        value: 100,
+    },
+    {
+        name: "Python Flask",
+        value: 100,
+    },
+    {
+        name: "Wordpress Plugin Development",
+        value: 100,
+    },
+    {
+        name: "Figma Designs",
+        value: 100,
+    },
+];
+
+const services = [{
+    title: "CMS & POS System",
+    description: "I specialize in developing custom CMS and admin portals for websites, tailored to various industries such as retail (POS systems), inventory management, schools, and hospital management systems. With expertise in Laravel, PHP, HTML, CSS, and jQuery, I create powerful, user-friendly solutions that streamline operations and offer seamless management of your digital assets. Whether you need a robust POS system, an efficient inventory tracker, or an intuitive school or hospital management portal, I deliver secure and scalable platforms designed to meet your business needs.",
+},
+    {
+        title: "Mobile Apps & Published on Play and Appstore",
+        description: "I create visually appealing and functional app screens using Flutter, based on Figma designs. My expertise extends to implementing a wide range of features, including API integrations, Stripe payment gateway, and Firebase services. Whether you need third-party APIs or custom back-end connectivity, I ensure seamless integration into your app. I also handle the complete app development process, from design to deployment, and can publish your app on both the Google Play Store and Apple App Store, providing you with a fully functional, market-ready solution.",
+    },
+    {
+        title: "Mern Stack Dynamic Website managed on admin portals",
+        description: "I specialize in building dynamic websites with seamless content management through an admin portal. Whether you're managing blog posts, job listings, projects, or articles, I can create a platform that allows you to easily update and control everything. I leverage cutting-edge front-end technologies like React, Vue.js, or Inertia.js, and combine them with robust back-end frameworks like Laravel, PHP, or Node.js for a scalable and high-performance solution. Additionally, I can Dockerize your application and deploy it on AWS EC2 instances or Elastic Beanstalk, ensuring a reliable, scalable, and production-ready environment.",
+    },
+    {
+        title: "Figma UI & UX Design",
+        description: "I specialize in creating sleek and intuitive designs for your app and website using Figma. With a focus on user experience and modern design principles, I deliver visually appealing layouts that are both functional and engaging. Whether you're building an app or a website, I will collaborate with you to bring your vision to life, ensuring the design reflects your brand and meets the needs of your users. From wireframes to high-fidelity prototypes, I create design solutions that are ready for development and implementation.",
+    },
+    {
+        title: "Wordpress Custom Plugins & Website Customization",
+        description: "I specialize in creating custom WordPress mockups and full-scale projects tailored to your specific needs. Whether you’re looking to design a new website or enhance an existing one, I can bring your vision to life with modern, user-friendly designs. Additionally, I develop custom WordPress plugins to extend your site's functionality, including travel booking systems, printing calculators, and much more. With my expertise, I’ll help you create a dynamic, feature-rich WordPress site that perfectly aligns with your goals.",
+    },
+    {
+        title: "Fix and deploy Servers Issue on Cpanel & Cloud Servers",
+        description: "I offer expert solutions for resolving server issues on cPanel and can efficiently deploy and manage your applications using AWS services. Whether you're dealing with server configuration problems or need deployment on AWS EC2 instances, Elastic Beanstalk, Route 53, S3, or other AWS services, I ensure smooth and secure setups. With extensive experience in cloud hosting and server management, I can help optimize your infrastructure, ensuring your website or application runs flawlessly.",
+    },
+    {
+        title: "Install SSL & domain DNS Setting",
+        description: "I provide services to set up free SSL certificates for your website, ensuring secure connections and enhancing user trust. Whether you need assistance with domain DNS settings or hosting DNS configurations, I will guide you through the entire process. With my expertise, I will help you secure your website, protect sensitive information, and improve your search engine rankings by implementing SSL effectively.",
+    },
+];
+
+const dataportfolio = [{
+    img: "aw.png",
+    description: "I developed a web application with Laravel PHP and Inertia.js that features three admin portals where users can buy and sell cars. The platform offers a seamless and secure experience for managing transactions and listings.",
+    link: "#",
+},
+    {
+        img: "fut.png",
+        description: "I developed a food ordering mobile app using Flutter, providing users with a seamless and intuitive interface for browsing menus and placing orders. The app leverages a Laravel API for efficient order management and real-time updates.",
+        link: "#",
+    },
+    {
+        img: "react.png",
+        description: "I created a gym website using React, where users can view membership details, learn how to burn calories, and access fitness tips. The site is designed to provide an engaging and informative experience for fitness enthusiasts.",
+        link: "#",
+    },
+    {
+        img: "cos.png",
+        description: "I built a business website using the MERN stack, with a Node.js API for back-end management and a React front end. The admin portal enables efficient management of all website operation like job posting , providing a seamless experience for users.\n" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "\n",
+        link: "#",
+    },
+    {
+        img: "belize.png",
+        description: "I created an admin portal for a water taxi service using Laravel PHP, featuring ticket and cargo management with tracking via parcel IDs and QR code scanning. The application is built with jQuery and JavaScript, deployed in a Docker environment for streamlined operations.",
+        link: "#",
+    },
+    {
+        img: "sales.png",
+        description: "I developed a POS inventory system similar to Sales Pro, offering robust features for managing sales, stock levels, and customer transactions. The system is designed for ease of use, ensuring efficient inventory tracking and streamlined sales processes.",
+        link: "#",
+    },
+    {
+        img: "tm.png",
+        description: "I developed a responsive website using HTML, CSS, JavaScript, and PHP, allowing users to purchase and book services such as haircuts, cooking classes, and more. The platform provides a user-friendly experience for seamless service selection and booking",
+        link: "#",
+    },
+
+
+    {
+        img: "point.png",
+        description: "I developed a doctor appointment portal using Laravel PHP, allowing users to book appointments and pay fees directly to doctors, along with a chat system using Pusher for real-time communication. This platform enhances user experience by facilitating easy access to medical consultations and support.",
+        link: "#",
+    },
+    {
+        img: "olivrad.png",
+        description: "I developed a Flutter app and a corresponding website for a radio station, utilizing a WordPress API to manage content and integrating music widgets using third-party JavaScript CDNs. This setup offers a seamless user experience for streaming music and accessing radio features across both platforms.",
+        link: "#",
+    },
+    {
+        img: "agol.png",
+        description: "I developed a logistics admin portal using Laravel PHP, focused on managing plane logistics and enabling efficient parcel tracking through unique tracking IDs. The platform enhances operational oversight and streamlines the logistics process for administrators.",
+        link: "#",
+    },
+    {
+        img: "msic.png",
+        description: "I created a music website using CodeIgniter, allowing artists to sell their albums directly to fans through a user-friendly interface. The platform features an admin portal for efficient management of albums, sales, and user interactions.",
+        link: "#",
+    },
+    {
+        img: "geo.png",
+        description: "I developed a map application with a web view using Flutter, allowing users to access detailed information about destinations and routing via the Google Maps API. The backend is built with Laravel PHP to efficiently manage data and enhance the user experience.",
+        link: "#",
+    },
+    {
+        img: "ecommerce.png",
+        description: "I created an e-commerce website on Shopify specializing in leather jackets, featuring product listings with multiple variants for customers to choose from. The platform is designed to provide an intuitive shopping experience while effectively showcasing the product range.",
+        link: "#",
+    },
+    {
+        img: "fitness.png",
+        description: "I developed a fitness admin portal using Laravel PHP, where users can purchase training packages and receive personalized guidance on diet and exercise routines. The portal includes features to track recommended push-ups and other exercises, enhancing user engagement and fitness outcomes.",
+        link: "#",
+    },
+    {
+        img: "eca.png",
+        description: "I developed a shopping project using Laravel PHP, Pusher, and Docker, deployed on AWS EC2, allowing users to engage in online shopping for various products, similar to Amazon. The platform features real-time notifications and a seamless shopping experience for customers.",
+        link: "#",
+    },
+    {
+        img: "va.png",
+        description: "I created a drink portal using Laravel PHP and React, enabling users to purchase drinks and place orders that are sent to restaurants for fulfillment. The system also facilitates order delivery to riders, streamlining the entire ordering and delivery process.",
+        link: "#",
+    },
+    {
+        img: "real.png",
+        description: "I developed a real estate portal using Laravel PHP, where admins can create posts and schedule them for publication on social media on specific dates. The platform also allows for easy management of listings and estates, enhancing visibility and engagement for real estate offerings.",
+        link: "#",
+    },
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "hammadhussain@gmail.com",
+    YOUR_FONE: "(+92)3150-234884",
+    description: "",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/HammadKhadimHussain",
+    youtube: "https://www.youtube.com/@ProgrammingAvengers1",
+    linkedin: "https://linkedin.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
